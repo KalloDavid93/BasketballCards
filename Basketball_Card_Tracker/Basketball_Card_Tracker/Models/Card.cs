e88209e4171b810a3aee9963941bdeb56b67ae9d
KalloDavid93 <kallo.david93@gmail.com>
2020-01-23 15:48:00 +0100

    card classes made

---
+++ Basketball_Card_Tracker/Basketball_Card_Tracker/Models/Card.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basketball_Card_Tracker.Models
+{
+    class Card
+    {
+    }
+}
---
+++ Basketball_Card_Tracker/Basketball_Card_Tracker/Models/CardTrade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basketball_Card_Tracker.Models
+{
+    class CardTrade : Card
+    {
+        public int Quantity { get; set; }
+    }
+}
